c4b580a32380b09f630524919fe4c0727bff3193
Brian Stack <bis12@case.edu>
2011-05-13 17:20:46 -0400

    Add relationship between talks and accounts
    This is used for api keys for embedding video

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 4) do
 
   create_table "accounts", :force => true do |t|
     t.string "name"
@@ -18,18 +18,21 @@ ActiveRecord::Schema.define(:version => 2) do
     t.string "email"
     t.string "crypted_password"
     t.string "role"
+    t.string "vid_uname"
+    t.string "vid_pass"
   end
 
   create_table "talks", :force => true do |t|
-    t.string "title"
-    t.string "speaker"
-    t.date   "date"
-    t.string "s_descrip"
-    t.string "l_descrip"
-    t.string "thumb"
-    t.string "pics"
-    t.string "video"
-    t.string "slides"
+    t.string  "title"
+    t.string  "speaker"
+    t.date    "date"
+    t.string  "s_descrip"
+    t.string  "l_descrip"
+    t.string  "thumb"
+    t.string  "pics"
+    t.string  "video"
+    t.string  "slides"
+    t.integer "account_id"
   end
 
 end

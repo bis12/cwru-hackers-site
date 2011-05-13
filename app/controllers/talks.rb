d0782011518c76a9e3e1a888450362a92f39e672
Brian Stack <bis12@case.edu>
2011-05-13 17:44:24 -0400

    Add roughly working video embed
    No upload yet

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -1,19 +1,18 @@
 CwruHackersSite.controllers :talks do
 
-  before do
-  	@viddler = Viddler::Base.new('1j4mmztdfn16ihsw934a')
-  end
-
   get :index do
+  	  @viddler = Viddler::Base.new('1j4mmztdfn16ihsw934a', acc)
 	  @talks = Talk.all
 	  render 'talks/index'
   end
 
   get :talk, :map => '/talks/:id' do
 	  if Talk.exists? params[:id] 
-	  	@talk = Talk.find params[:id]
+		@talk = Talk.find params[:id]
+		account = @talk.account
+  	  	@viddler = Viddler::Base.new('1j4mmztdfn16ihsw934a', account.vid_uname, account.vid_pass)
 		to_emb = @viddler.find_video_by_id  @talk.video
-		@embed = to_emb.embed_code
+		@embed = to_emb.embed_code :player_type => 'simple'
 	  	render 'talks/talk'
 	  else
 		  render 'talks/notalk'
